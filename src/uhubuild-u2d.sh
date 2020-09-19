7999cb11ecb43a6a7cae21051b802888c2e8faba
rezso <rezso@rezso.net>
2020-09-19 18:53:31 +0200

    uhubuild-u2d: u2dgnome url csere

--- src/uhubuild-u2d.sh
+++ src/uhubuild-u2d.sh
@@ -141,8 +141,10 @@ function u2dgnome {
 	if [ -n "${unstable:-}" ]; then
 		reg="0-9"
 	fi
-	ver="$(urllist "https://download.gnome.org/sources/$project/" | grep -E '^[0-9]+\.[0-9]*['$reg'](\.[0-9.])?$' | sort -V | tail -n 1)"
-	u2d "https://download.gnome.org/sources/$project/$ver"
+	ver="$(urllist "https://mirrors.gethosted.online/gnome/sources/$project/" | grep -E '^[0-9]+\.[0-9]*['$reg'](\.[0-9.])?$' | sort -V | tail -n 1)"
+	u2d "https://mirrors.gethosted.online/gnome/sources/$project/$ver"
+#	ver="$(urllist "https://download.gnome.org/sources/$project/" | grep -E '^[0-9]+\.[0-9]*['$reg'](\.[0-9.])?$' | sort -V | tail -n 1)"
+#	u2d "https://download.gnome.org/sources/$project/$ver"
 }
 
 # script for mate projects
